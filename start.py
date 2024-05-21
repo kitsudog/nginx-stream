a45527d145e1882962a349269b43226203150ccb
dave2023 <dave2023@googlemail.com>
2024-05-22 00:47:30 +0800

    feat: 增加默认转发

--- start.py
+++ start.py
@@ -33,7 +33,7 @@ def render(template, *, default: Dict = {}, **kwargs):
 def gen_nginx_config(listen_config_list, stream_config_list, proxy_config_list, redirect_config_list,
                      tunnel_config_list, listen_port=80,
                      dns="8.8.8.8", config_file="/etc/nginx/nginx.conf", client_size="10m", external_host="$http_host",
-                     external_proto="http", proxy_listen_port=82, disable_proxy="FALSE"):
+                     external_proto="http", proxy_listen_port=82, disable_proxy="FALSE", default_forward=False):
     disable_proxy = str(disable_proxy).lower() in {"true", "1"}
     kwargs = locals().copy()
     tmp = {}
